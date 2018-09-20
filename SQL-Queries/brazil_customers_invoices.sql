e1e1fc2f8260393f5891aaa04b14fdb5a25fb320
Aaron Miller <millerac333@gmail.com>
2018-09-20 18:22:35 -0500

    successfully pulled Brazilan Customer Invoices with SQL query

---
+++ SQL-Queries/brazil_customers_invoices.sql
@@ -0,0 +1,5 @@
+-- Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+SELECT FirstName, LastName, Invoice.InvoiceId, Invoice.InvoiceDate, BillingCountry
+FROM Customer 
+JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
+WHERE Country = 'Brazil';
