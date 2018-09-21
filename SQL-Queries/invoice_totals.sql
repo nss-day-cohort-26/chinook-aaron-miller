d44fe15b716cf6ee4a16e617cabd026054f94276
Aaron Miller <millerac333@gmail.com>
2018-09-21 11:06:04 -0500

    adjusted indent syntax

--- SQL-Queries/invoice_totals.sql
+++ SQL-Queries/invoice_totals.sql
@@ -1,6 +1,9 @@
 -- Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
 SELECT Invoice.Total, Customer.FirstName || " " || Customer.LastName 
-AS CustomerFullName, Customer.Country, Employee.FirstName || " " || Employee.LastName AS EmployeeFullName, *
+AS CustomerFullName, Customer.Country, Employee.FirstName || " " || Employee.LastName AS EmployeeFullName,
+    *
 FROM Customer
-    JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
-    JOIN Employee ON Employee.EmployeeId = Customer.SupportRepId;
+    JOIN Invoice
+    ON Invoice.CustomerId = Customer.CustomerId
+    JOIN Employee
+    ON Employee.EmployeeId = Customer.SupportRepId;
