df477ca26ddb48bc5c0939fa6c2d71f19d0b21ec
Aaron Miller <millerac333@gmail.com>
2018-09-24 08:13:51 -0500

    query will pull data of total sale in respective years of 2009 and 2011 from InvoiceTable

--- SQL-Queries/total_sales_{year}.sql
+++ SQL-Queries/total_sales_{year}.sql
@@ -1,1 +1,12 @@
 -- What are the respective total sales for each of those years (2009,2011)?
+    SELECT
+        SUM(Invoice.Total)
+    FROM Invoice
+    WHERE Invoice.InvoiceDate 
+LIKE '2009%'
+UNION ALL
+    SELECT
+        SUM(Invoice.Total)
+    FROM Invoice
+    WHERE Invoice.InvoiceDate 
+LIKE '2011%';
